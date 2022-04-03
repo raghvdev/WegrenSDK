9b375bc8ff33b2c80128dff6e21aecb1b197b2aa
raghvdev <102845487+raghvdev@users.noreply.github.com>
2022-04-03 12:03:14 +0530

    Update MainActivity.java
    commit

--- app/src/main/java/com/wegren/wegrensdk/MainActivity.java
+++ app/src/main/java/com/wegren/wegrensdk/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WegrenApp wegrenApp = new WegrenApp();
-        wegrenApp.systemInformationSent(this, "1","2");
+//        WegrenApp wegrenApp = new WegrenApp();
+//        wegrenApp.systemInformationSent(this, "1f7d768a-970f-4c3b-b83c-0a554fa0d8ed","5100aebb-b4f3-4b0b-a82f-2f8c388f9513");
     }
 }
